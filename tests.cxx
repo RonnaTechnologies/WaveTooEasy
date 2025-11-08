8c0711e322cb393f1f17cd5731bae8507f15698f
RonnaTechnologies <contact@ronnatech.com>
2025-11-08 22:40:23 +0100

    improve test

--- tests.cxx
+++ tests.cxx
@@ -3,12 +3,27 @@
 
 #include "include/FileSystem.hpp"
 
+#include <string_view>
+
+
 SCENARIO("Sound files are parsed successfully")
 {
     GIVEN("sound files names")
     {
-        fs::FileSystem dir{ "../data" };
-        [[maybe_unused]] const auto list = dir.list_files();
-        [[maybe_unused]] auto a = 1;
+        const auto dir = fs::FileSystem{ "../data" };
+        const auto list = dir.list_files();
+
+        for (const auto& file_name : list)
+        {
+            const auto slash_pos = static_cast<std::ptrdiff_t>(file_name.find_last_of('/'));
+            const auto name = std::string_view{ file_name.begin() + slash_pos + 1, file_name.end() - 4 };
+
+            const auto dash_pos = static_cast<std::ptrdiff_t>(name.find_first_of('-'));
+
+            const auto note_str = std::string_view{ name.begin(), name.begin() + dash_pos };
+            const auto index_str = std::string_view{ name.begin() + dash_pos + 1, name.end() };
+
+            [[maybe_unused]] auto a = 1;
+        }
     }
 }
