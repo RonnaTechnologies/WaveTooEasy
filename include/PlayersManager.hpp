dc293306f9fad854adca4f9ac3202fda45ccc733
RonnaTechnologies <contact@ronnatech.com>
2025-11-27 16:22:50 +0100

    improve player manager algorithm

--- include/PlayersManager.hpp
+++ include/PlayersManager.hpp
@@ -22,8 +22,6 @@ namespace proc
         {
             const auto now = get_time();
 
-            std::sort(slots.begin(), slots.end(), [](const note_time& lhs, const note_time& rhs)
-                      { return (lhs.start_time + lhs.length) < (rhs.start_time + rhs.length); });
 
             // look for a free slot
             const auto slot_it =
@@ -38,7 +36,7 @@ namespace proc
             return static_cast<std::size_t>(-1);
         }
 
-        auto insert_note(const std::uint8_t note, const std::uint32_t duration) -> bool
+        auto insert_note(const std::uint8_t note, const std::uint8_t velocity, const std::uint32_t duration) -> bool
         {
             const auto index = compute_index_for(note);
             if (index == static_cast<std::size_t>(-1))
@@ -46,14 +44,24 @@ namespace proc
                 return false;
             }
 
-            slots[index] = note_time{ .note = note, .start_time = get_time(), .length = duration };
+            slots[index] = note_time{ .note = note, .velocity = velocity, .start_time = get_time(), .length = duration };
+
+            std::sort(slots.begin(), slots.end(), [](const note_time& lhs, const note_time& rhs)
+                      { return (lhs.start_time + lhs.length) < (rhs.start_time + rhs.length); });
+
             return true;
+        }
+
+        [[nodiscard]] auto& get_slots() const noexcept
+        {
+            return slots;
         }
 
     private:
         struct note_time
         {
             std::uint8_t note{};
+            std::uint8_t velocity{};
             std::uint32_t start_time{};
             std::uint32_t length{};
         };
