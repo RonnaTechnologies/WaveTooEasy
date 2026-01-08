6bfb9247d11a57a20323d051aac43c012a98622b
RonnaTechnologies <contact@ronnatech.com>
2026-01-08 22:35:48 +0100

    improve voice stealing algorithm

--- include/PlayersManager.hpp
+++ include/PlayersManager.hpp
@@ -36,25 +36,12 @@ namespace proc
 
         auto insert_note(const std::uint8_t note, const std::uint8_t velocity, const bool force = false) -> int
         {
-            const auto add_note = [&](const auto& index) -> std::size_t
-            {
-                slots[index] = note_time{ .note = note, .velocity = velocity, .start_time = get_time() };
-                return index;
-            };
-
             const auto now = get_time();
-            const auto index = find_free_slot(now);
-            const auto success = index != static_cast<std::size_t>(-1);
 
-
-            if (!success && !force)
+            if (const auto index = find_free_slot(now); index != static_cast<std::size_t>(-1))
             {
-                return -1;
-            }
-
-            if (success)
-            {
-                return add_note(index);
+                slots[index] = note_time{ note, velocity, now, 0 };
+                return static_cast<int>(index);
             }
 
             if (!force)
@@ -62,46 +49,52 @@ namespace proc
                 return -1;
             }
 
-            // force
-
-            const auto nb_same_notes =
-            std::count_if(slots.cbegin(), slots.cend(), [&](const note_time& n_t) { return n_t.note == note; });
-
-            const auto get_impulse = [](const note_time& n_t, const std::uint32_t& now) -> float
+            const auto get_priority = [now](const note_time& n_t) -> float
             {
                 const auto start_impulse = std::uint32_t{ n_t.velocity } * std::uint32_t{ n_t.duration };
-                return static_cast<float>(start_impulse) - ((now - n_t.start_time) * static_cast<float>(n_t.velocity));
+                const auto elapsed_impulse = (now - n_t.start_time) * std::uint32_t{ n_t.velocity };
+                return static_cast<float>(start_impulse) - static_cast<float>(elapsed_impulse);
             };
 
-            if (nb_same_notes == 1)
+            const auto is_same_note = [note](const note_time& n_t) { return n_t.note == note; };
+
+            const auto same_note_it = std::find_if(slots.begin(), slots.end(), is_same_note);
+
+            if (same_note_it != slots.end())
             {
-                const auto same_note_it =
-                std::find_if(slots.cbegin(), slots.cend(), [&](const note_time& n_t) { return n_t.note == note; });
-                const auto& same_note = *same_note_it;
+                const auto lowest_same = std::min_element(slots.begin(), slots.end(),
+                                                          [note, &get_priority](const note_time& lhs, const note_time& rhs)
+                                                          {
+                                                              const auto lhs_is_same = (lhs.note == note);
+                                                              const auto rhs_is_same = (rhs.note == note);
 
-                const auto sn_impulse = get_impulse(same_note, now);
+                                                              if (lhs_is_same && rhs_is_same)
+                                                              {
+                                                                  return get_priority(lhs) < get_priority(rhs);
+                                                              }
+                                                              return lhs_is_same && !rhs_is_same;
+                                                          });
 
-                const auto note_impulse = static_cast<float>(std::uint32_t{ velocity } * std::uint32_t{ same_note.duration });
-
-                if (note_impulse > sn_impulse)
-                {
-                    return add_note(std::distance(slots.cbegin(), same_note_it));
-                }
+                const auto steal_index = std::distance(slots.begin(), lowest_same);
+                slots[steal_index] = note_time{ note, velocity, now, 0 };
+                return static_cast<int>(steal_index);
             }
 
-            if (nb_same_notes >= 2)
-            {
-            }
+            const auto steal_it =
+            std::min_element(slots.begin(), slots.end(), [&get_priority](const note_time& lhs, const note_time& rhs)
+                             { return get_priority(lhs) < get_priority(rhs); });
 
-            return add_note(0);
+            const auto steal_index = std::distance(slots.begin(), steal_it);
+            slots[steal_index] = note_time{ note, velocity, now, 0 };
+            return static_cast<int>(steal_index);
         }
 
         void set_duration(const std::size_t& index, const std::uint32_t duration) noexcept
         {
             slots[index].duration = duration;
 
-            std::sort(slots.begin(), slots.end(), [](const note_time& lhs, const note_time& rhs)
-                      { return ((lhs.start_time + lhs.duration)) > ((rhs.start_time + rhs.duration)); });
+            // std::sort(slots.begin(), slots.end(),
+            //           [](const note_time& lhs, const note_time& rhs) { return lhs.velocity > rhs.velocity; });
         }
 
         [[nodiscard]] auto& get_slots() const noexcept
